1cf7e1e1b3381baeeb5e29c6acef658a306aa6bc
Cody Fussell <cody.fussell@gmail.com>
2019-09-06 20:01:40 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -27,7 +27,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if phone.match?(/^[A-Z].*[?.!]$/)
+  if phone.match?(/^\D?(\d{3})\D?\D?(\d{3})\D?(\d{4})$/)
     return true
   else
     return false
